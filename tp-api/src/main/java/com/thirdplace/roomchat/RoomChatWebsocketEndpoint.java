60be1991247fa578940d7c78cc194528be86c9d3
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2025-01-20 14:44:47 -0700

    logging and exceptions

--- tp-api/src/main/java/com/thirdplace/roomchat/RoomChatWebsocketEndpoint.java
+++ tp-api/src/main/java/com/thirdplace/roomchat/RoomChatWebsocketEndpoint.java
@@ -2,6 +2,9 @@ package com.thirdplace.roomchat;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.websocket.OnClose;
 import jakarta.websocket.OnError;
 import jakarta.websocket.OnMessage;
@@ -12,9 +15,12 @@ import jakarta.websocket.server.ServerEndpoint;
 @ServerEndpoint("/chat")
 public class RoomChatWebsocketEndpoint {
 
+    static final Logger LOGGER = LoggerFactory.getLogger(RoomChatWebsocketEndpoint.class);
+
     @OnOpen
     public void onOpen(final Session session) {
         try {
+            LOGGER.info("WebSocket connection opened: " + session.getId());
             // Send a welcome message to the client
             session.getBasicRemote().sendText("Welcome to the WebSocket server!");
         } catch (IOException e) {
@@ -25,19 +31,23 @@ public class RoomChatWebsocketEndpoint {
     @OnMessage
     public void onMessage(final String message, final Session session) {
         try {
+            LOGGER.debug("Message recieved " + message);
             session.getBasicRemote().sendText("Message received: " + message);
         } catch (IOException e) {
+            LOGGER.error("Error sending message", e);
             e.printStackTrace();
         }
     }
 
     @OnClose
     public void onClose(final Session session) {
+        LOGGER.info("WebSocket connection closed: " + session.getId());
     }
 
     @OnError
     public void onError(final Session session, final Throwable throwable) {
-        throwable.printStackTrace();
+        LOGGER.error("WebSocket error: " + session.getId(), throwable);
+        LOGGER.error("throwable.printStackTrace()");
     }
 
 }

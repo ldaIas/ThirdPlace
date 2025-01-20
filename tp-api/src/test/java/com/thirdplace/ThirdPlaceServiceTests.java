4b5e2fe23e498cd60fc76c23acbacff48274e8f7
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2025-01-19 23:35:02 -0700

    java init

---
+++ tp-api/src/test/java/com/thirdplace/ThirdPlaceServiceTests.java
@@ -0,0 +1,12 @@
+package com.thirdplace;
+
+import org.junit.jupiter.api.Test;
+
+public class ThirdPlaceServiceTests {
+    
+    @Test
+    void exampleTest() {
+        String result = "  Hello, Test!  ";
+        assertEquals("Hello, Test!", result.trim());
+    }
+}

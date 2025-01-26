a5a5016cfff425869c00174e42279a139fcf4562
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2025-01-26 14:11:55 -0700

    frontend page routing

---
+++ tp-frontend/src/LoginStyles.elm
@@ -0,0 +1,8 @@
+{- This file should only contain string constants to properties found in the appropriate stylesheets files
+   - This one goes with roomstyles.css
+-}
+module LoginStyles exposing (..)
+
+loginContainer : String
+loginContainer =
+    "login-container"

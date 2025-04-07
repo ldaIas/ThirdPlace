c16821fa14ee533bd1f29bb5ed49be7464b6ca7a
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2025-04-06 20:49:37 -0600

    idk man

--- tp-frontend/vite.config.js
+++ tp-frontend/vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     root: './', // wherever index.html is
     build: {
-        outDir: 'dist'
+        outDir: 'dist',
+        sourcemap: false
     },
     resolve: {
         dedupe: ['@kiltprotocol/core', '@kiltprotocol/did']

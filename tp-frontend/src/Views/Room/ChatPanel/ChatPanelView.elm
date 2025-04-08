8ad71c08108194b68473b260d5af7507c894a214
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2025-04-08 17:03:50 -0600

    intermit

--- tp-frontend/src/Views/Room/ChatPanel/ChatPanelView.elm
+++ tp-frontend/src/Views/Room/ChatPanel/ChatPanelView.elm
@@ -1,13 +1,37 @@
 module Views.Room.ChatPanel.ChatPanelView exposing (..)
 
-import Html exposing (Html, div, h2, text)
-import Html.Attributes exposing (class)
+import Html exposing (Html, button, div, h2, text, textarea)
+import Html.Attributes exposing (class, placeholder)
+import Html.Events exposing (onInput)
+import Views.Room.ChatPanel.ChatPanelStyles as ChatPanelStyles
 import Views.Room.RoomModel as RoomModel
 import Views.Room.RoomStyles as RoomStyles
-import Views.Room.ChatPanel.ChatPanelStyles as ChatPanelStyles
 
 
 view : RoomModel.Model -> Html msg
 view _ =
     div [ class RoomStyles.container, class ChatPanelStyles.chatPanel ]
-        [ h2 [] [ text "Selected Conversation" ] ]
+        [ div [ class "chat-container" ]
+            [ div [ class "chat-header" ]
+                [ div [ class "chat-topic" ] [ text "Topic: Future of Decentralized Social" ]
+                , div [ class "chat-author" ] [ text "Started by: codingcat" ]
+                ]
+            , div [ class "chat-messages" ]
+                [ div [ class "message from-other" ]
+                    [ div [ class "sender" ] [ text "user42" ]
+                    , div [ class "content" ] [ text "I really think peer-to-peer is the way forward." ]
+                    ]
+                , div [ class "message from-self" ]
+                    [ div [ class "sender" ] [ text "me" ]
+                    , div [ class "content" ] [ text "Exactly. LibP2P has been solid so far." ]
+                    ]
+                ]
+            , div [ class "chat-input" ]
+                [ textarea
+                    [ placeholder "Type your message..."
+                    ]
+                    []
+                , button [] [ text "Send" ]
+                ]
+            ]
+        ]
